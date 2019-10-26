94159f3962be1954ed5769f72b180bae8a6f040b
Peter Huang <peter.huang339@icloud.com>
2019-10-26 16:29:28 -0400

    init changes from ryley

---
+++ PitStop/PitStopGUI.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+import simulate
+root = tk.Tk()
+logo = tk.PhotoImage(file="pictures/docbutton.gif")
+run = tk.PhotoImage(file="pictures/runButton.gif")
+checkIn = tk.PhotoImage(file="pictures/checkInButton.gif")
+end = tk.PhotoImage(file="pictures/endButton.gif")
+root.configure(background="white")
+root.geometry("800x500") #Width x Height
+w = tk.Label(root, compound=tk.CENTER,  image=logo).grid(padx=10, row=0, column=0)
+
+runbutton = tk.Button(root, image=run, command=lambda: onclick(1)).grid(padx=10, row=0, column=1)
+checkINbutton = tk.Button(root, image=checkIn, command=lambda: onclick(2)).grid(padx=10, row=0, column=2)
+endbutton = tk.Button(root, image=end, command=lambda: onclick(3)).grid(padx=10, row=0, column=3)
+
+
+def onclick(args):
+    doctorid = ""
+    if args == 1:
+        print("yoooo")
+    if args == 2:
+        doctorid = ""
+        label_ci = tk.Label(root, text="Please enter your doctor ID").grid(padx=30, row=1, column=2)
+        entry_ci = tk.Entry(root, bd=3).grid(padx=30, row=2, column=2)
+        button_ci = tk.Button(root, text="ENTER", command=getdoctor()).grid(row=1, column=3)
+    if args == 3:
+        exit()
+    print(doctorid)
+def getdoctor():
+    score = simulate.simulate().score
+    print(score)
+root.mainloop()
+
+
